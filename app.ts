072d29f8955f98d60caafc6867b6f2634c787572
Guilherme Gades Vargas <guilherme.gades@gmail.com>
2021-04-20 15:08:49 -0300

    allowing cors for localhost request

--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 import router from './routes.ts'
 
 const { args } = Deno;
 const argPort = parse(args).port;
-const HOST = '127.0.0.1'
 const PORT = argPort ? Number(argPort) : 8000
 const app = new Application()
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+app.use(
+  oakCors({
+    origin: 'http://localhost:3000'
+  }),
+);
 
 console.log(`Listening on port ${PORT} ...`)
 await app.listen({ port: PORT })
